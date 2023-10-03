dc8a233dd5f9447ff9648ca2193f33ab4671b48d
Avishai Cohen <avishaic2016@gmail.com>
2023-10-03 21:19:24 +0300

    adding opening details on stay card

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom"
+import { useNavigate } from 'react-router-dom'
 
 export function StayPreview({ stay }) {
+
+  const navigate = useNavigate()
+
+  function onStay(stayId) {
+    return navigate(`/stay/${stayId}`)
+  }
+
   return (
-    <section className='stay-preview'>
+    <section className='stay-preview' onClick={() => onStay(stay._id)} >
       <img src={stay.imgUrls[0]} />
       <div className='stay-loc'>{stay.loc.city}, {stay.loc.country}</div>
       <div className='stay-price'>${stay.price} <span>total</span></div>
-      <Link className="stay-details" to={`/stay/${stay._id}`}>Details</Link>
+      {/* <Link className="stay-details" to={`/stay/${stay._id}`}>Details</Link> */}
     </section>
   )
 }
