83296d8eab613b792dd405fd423f0c91b705dd81
OriDalm <oridalmedigos@gmail.com>
2023-10-03 16:15:14 +0300

    Refactored to stay

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,50 +5,50 @@ import { ReviewIndex } from './pages/ReviewIndex.jsx'
 import { ChatApp } from './pages/Chat.jsx'
 import { AdminApp } from './pages/AdminIndex.jsx'
 import { SurveyIndex } from './pages/SurveyIndex.jsx'
-import { BabaIndex } from './pages/BabaIndex.jsx'
+import { StayIndex } from './pages/StayIndex.jsx'
 
 // Routes accesible from the main navigation (in AppHeader)
 const routes = [
-    {
-        path: '/',
-        component: <HomePage />,
-        label: 'Home 🏠',
-    },
-    {
-        path: 'baba',
-        component: <BabaIndex />,
-        label: 'Meet our Babas'
-    },
-    // {
-    //     path: 'survey',
-    //     component: <SurveyIndex />,
-    //     label: 'Take our survey'
-    // },
-    {
-        path: 'car',
-        component: <CarIndex />,
-        label: 'Cars'
-    },
-    // {
-    //     path: 'review',
-    //     component: <ReviewIndex />,
-    //     label: 'Reviews'
-    // },
-    // {
-    //     path: 'chat',
-    //     component: <ChatApp />,
-    //     label: 'Chat'
-    // },
-    // {
-    //     path: 'about',
-    //     component: <AboutUs />,
-    //     label: 'About us'
-    // },
-    // {
-    //     path: 'admin',
-    //     component: <AdminApp />,
-    //     label: 'Admin Only'
-    // }
+  {
+    path: '/',
+    component: <HomePage />,
+    label: 'Home 🏠',
+  },
+  // {
+  //     path: 'survey',
+  //     component: <SurveyIndex />,
+  //     label: 'Take our survey'
+  // },
+  {
+    path: 'car',
+    component: <CarIndex />,
+    label: 'Cars',
+  },
+  {
+    path: 'stay',
+    component: <StayIndex />,
+    label: 'Stays',
+  },
+  // {
+  //     path: 'review',
+  //     component: <ReviewIndex />,
+  //     label: 'Reviews'
+  // },
+  // {
+  //     path: 'chat',
+  //     component: <ChatApp />,
+  //     label: 'Chat'
+  // },
+  // {
+  //     path: 'about',
+  //     component: <AboutUs />,
+  //     label: 'About us'
+  // },
+  // {
+  //     path: 'admin',
+  //     component: <AdminApp />,
+  //     label: 'Admin Only'
+  // }
 ]
 
 export default routes
