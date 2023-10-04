66a963a234807c66235b304d3807fadb6c0892f0
OriDalm <oridalmedigos@gmail.com>
2023-10-04 13:27:15 +0300

    Header before design

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -1,8 +1,7 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 
 export function StayPreview({ stay }) {
-
   const navigate = useNavigate()
 
   function onStay(stayId) {
@@ -10,10 +9,14 @@ export function StayPreview({ stay }) {
   }
 
   return (
-    <section className='stay-preview' onClick={() => onStay(stay._id)} >
+    <section className='stay-preview' onClick={() => onStay(stay._id)}>
       <img src={stay.imgUrls[0]} />
-      <div className='stay-loc'>{stay.loc.city}, {stay.loc.country}</div>
-      <div className='stay-price'>${stay.price} <span>total</span></div>
+      <div className='stay-loc'>
+        {stay.loc.city}, {stay.loc.country}
+      </div>
+      <div className='stay-price'>
+        ${stay.price} <span>total</span>
+      </div>
       {/* <Link className="stay-details" to={`/stay/${stay._id}`}>Details</Link> */}
     </section>
   )
