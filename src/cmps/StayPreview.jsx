555a200fee638200632aaf1c95e786e7d1002763
Avishai Cohen <avishaic2016@gmail.com>
2023-10-04 13:36:42 +0300

    adding preview rating, distance, date

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom"
 import { useNavigate } from 'react-router-dom'
 
+import starSvg from '../assets/img/star.svg'
+
 export function StayPreview({ stay }) {
 
   const navigate = useNavigate()
@@ -11,10 +13,16 @@ export function StayPreview({ stay }) {
 
   return (
     <section className='stay-preview' onClick={() => onStay(stay._id)} >
-      <img src={stay.imgUrls[0]} />
-      <div className='stay-loc'>{stay.loc.city}, {stay.loc.country}</div>
-      <div className='stay-price'>${stay.price} <span>total</span></div>
-      {/* <Link className="stay-details" to={`/stay/${stay._id}`}>Details</Link> */}
+      <img className="stay-img" src={stay.imgUrls[0]} />
+      <div className='stay-loc'>Herzliya, Isrrael</div>
+      {/* <div className='stay-loc'>{stay.loc.city}, {stay.loc.country}</div> */}
+      <div className="stay-rating">
+        <img src={starSvg} />5
+      </div>
+      <p className='stay-distance'>11,855 km away from</p>
+      <p className='stay-date'>Oct 24-27</p>
+      {/* <div className='stay-price'>${stay.price} <span>total</span></div> */}
+      <div className='stay-price'>$6,084 <span>total</span></div>
     </section>
   )
 }
