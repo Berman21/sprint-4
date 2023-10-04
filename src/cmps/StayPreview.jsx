d0d41249cabdea3d1741eeebffec08eed139fe17
Avishai Cohen <avishaic2016@gmail.com>
2023-10-04 22:27:26 +0300

    adding Wishlist Icon toggle

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
+
 
 import starSvg from '../assets/img/star.svg'
 import heartSvg from '../assets/img/heart.svg'
@@ -8,40 +9,39 @@ import { WishlistIcon } from './WishlistIcon'
 
 export function StayPreview({ stay }) {
 
-  const [onWishlist, setonWishlist] = useState(false)
   const navigate = useNavigate()
+  const [setClr, onSetClr] = useState('#00000080')
 
   function onStay(stayId) {
     return navigate(`/stay/${stayId}`)
   }
 
-  function setWishlist() {
-    if (onWishlist) {
-      setonWishlist(false)
-      return '#00000080'
+  function onWishlistIcon() {
+    if (setClr === '#00000080') {
+      onSetClr('#ff385c')
     } else {
-      setonWishlist(true)
-      return '#ff385c'
+      onSetClr('#00000080')
     }
   }
 
   return (
-    <section className='stay-preview' onClick={() => onStay(stay._id)} >
+    <Fragment>
+      <WishlistIcon onWishlistIcon={onWishlistIcon} setClr={setClr} />
+      <section className='stay-preview' onClick={() => onStay(stay._id)} >
 
-      <WishlistIcon setWishlist={setWishlist} />
-      <img className="stay-img" src={stay.imgUrls[0]} />
+        <img className='stay-img' src={stay.imgUrls[0]} />
 
-      <div className='stay-loc'>Herzliya, Israel</div>
-      {/* <div className='stay-loc'>{stay.loc.city}, {stay.loc.country}</div> */}
-      <div className="stay-rating">
-        <img src={starSvg} />5.0
-      </div>
-      <p className='stay-distance'>11,855 km away from</p>
-      <p className='stay-date'>Oct 24-27</p>
+        <div className='stay-loc'>Herzliya, Israel</div>
+        {/* <div className='stay-loc'>{stay.loc.city}, {stay.loc.country}</div> */}
 
-      <div className='stay-price'>$6,084 <span>total</span></div>
-      {/* <div className='stay-price'>${stay.price} <span>total</span></div> */}
+        <div className='stay-rating'><img src={starSvg} />5.0</div>
+        <p className='stay-distance'>13 miles to Ramat Gan National Park</p>
+        <p className='stay-date'>6-1 Oct Individual Host </p>
 
-    </section>
+        <div className='stay-price'>£1,187 <span>night</span></div>
+        {/* <div className='stay-price'>${stay.price} <span>total</span></div> */}
+
+      </section>
+    </Fragment>
   )
 }
