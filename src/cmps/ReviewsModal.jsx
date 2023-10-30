3acf750bb4f2f6bbd679ea3e95c4d4fc2ae5e233
OriDalm <oridalmedigos@gmail.com>
2023-10-30 14:37:23 +0200

    Added reviews modal

--- src/cmps/ReviewsModal.jsx
+++ src/cmps/ReviewsModal.jsx
@@ -1,12 +1,101 @@
+import { useState } from 'react'
 import starSvg from '../assets/img/star.svg'
+import closeFilter from '../assets/img/close-filter.svg'
+import reviewsSearch from '../assets/img/reviews-search.svg'
 
 export function ReviewsModal({ stay }) {
+    const [searchTerm, setSearchTerm] = useState('')
+    const [filteredReviews, setFilteredReviews] = useState(stay.reviews)
+    const [mentionCount, setMentionCount] = useState(0)
+    const [mention, setMention] = useState('')
+    const [tempSearchTerm, setTempSearchTerm] = useState('')
+    const [isNoResult, setIsNoResult] = useState(true);
+
+
+    const handleSearch = () => {
+        const lowerCaseSearchTerm = tempSearchTerm.toLowerCase()
+        const filtered = stay.reviews.filter((review) =>
+            review.txt.toLowerCase().includes(lowerCaseSearchTerm)
+        )
+        setMention(lowerCaseSearchTerm)
+        setFilteredReviews(filtered)
+        setMentionCount(filtered.length)
+        setSearchTerm(tempSearchTerm)
+        if (!mentionCount) setIsNoResult(false)
+    }
+
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            handleSearch()
+        }
+    }
+
+    const handleSearchInputChange = (e) => {
+        setTempSearchTerm(e.target.value)
+    }
+
+    const handleClear = () => {
+        setTempSearchTerm('')
+        setSearchTerm('')
+        setFilteredReviews(stay.reviews)
+        setMentionCount(0)
+        setMention('')
+        setIsNoResult(true)
+    }
+
     return (
         <section className="modal-reviews-container">
             <section className='modal-reviews-score'>
-                <img src={starSvg} />
+                <img src={starSvg} alt="Star" />
+                <span>{stay.rating} · {stay.reviews.length} reviews</span>
             </section>
+            <section className='modal-reviews-list'>
+                <section>
+                    <div className='review-search'>
+                        <img src={reviewsSearch} />
+                        <input
+                            type="text"
+                            placeholder="Search reviews"
+                            value={tempSearchTerm}
+                            onChange={handleSearchInputChange}
+                            onKeyPress={handleKeyPress}
+                        />
+                        {tempSearchTerm &&
+                            <button className='btn-clear-filter' onClick={handleClear}><img src={closeFilter} /></button>}
+                    </div>
+                </section>
+                <div>
+                    {mentionCount > 0 && mention && (
+                        <p className='reviews-mentioned'>{mentionCount} reviews mentioned "{searchTerm}"</p>
+                    )}
+                    {!isNoResult && !mentionCount && (
+                        <p className='reviews-mentioned'>There are no results for "{searchTerm}"</p>
+                    )}
+                </div>
+                {filteredReviews.map((review, idx) => {
+                    const reviewDate = new Date(review.at)
+                    const month = reviewDate.toLocaleString('default', { month: 'long' })
+                    const year = reviewDate.getFullYear()
 
+                    return (
+                        <section className='modal-review-preview' key={idx}>
+                            <article className='modal-review-user'>
+                                <section className='modal-review-img'>
+                                    <img src={review.by.imgUrl} alt="User Image" />
+                                </section>
+                                <section className="review-user-details">
+                                    <h3>{review.by.fullname}</h3>
+                                    <span>{`${month} ${year}`}</span>
+                                </section>
+                            </article>
+                            <article className='modal-review-content'>
+                                <p>{review.txt}</p>
+                            </article>
+                        </section>
+                    )
+                })
+                }
+            </section>
         </section>
     )
 }
