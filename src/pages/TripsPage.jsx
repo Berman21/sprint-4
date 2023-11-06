c8eca61181e49a11c6be3571f81ea33493fb76a0
OriDalm <oridalmedigos@gmail.com>
2023-11-06 16:21:24 +0200

    Finished trips page design

--- src/pages/TripsPage.jsx
+++ src/pages/TripsPage.jsx
@@ -6,6 +6,7 @@ import { DashboardNav } from "../cmps/DashboardNav"
 import userIcon from '../assets/img/user.svg'
 
 
+
 export function TripsPage() {
     const loggedInUser = useSelector((storeState) => storeState.userModule.user)
     const [orders, setOrders] = useState([])
@@ -27,65 +28,49 @@ export function TripsPage() {
         }
     }
 
-    return (
+    function handleClick(orderId) {
+        navigate(`/stay/${orderId}`)
+    }
 
+    return (
         <section className="trips-page-container">
-            {/* <div>{orders && orders.map((order, idx) => (
-                <div key={idx}>{order.buyer.fullname}</div>
-            ))}</div> */}
             <section className="trips-header">
                 <h1>Trips</h1>
-                <img src={userIcon} />
-
+                <img src={userIcon} alt="User Icon" />
             </section>
             <DashboardNav />
             <section className="trips-container">
-
-                {orders && orders.map((order, idx) => (
-                    <div key={idx} className="blog-container">
-
-
-                        <div className="blog-header">
-                            <div className="blog-cover">
-                                <img src={order.stay.imgUrls[0]} />
-                                <div className="blog-author">
-                                    {/* <h3>{order.buyer.fullname}</h3> */}
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="blog-body">
-                            <div className="blog-title">
-                                <h1>{order.stay.name}</h1>
-                            </div>
-                            <div className="blog-summary">
-
-                                <li>{order.stay.roomType} hosted by {order.stay.hostFullname}</li>
-                                <li>{order.stay.city}, {order.stay.country}</li>
-                            </div>
-                            <div className="blog-tags">
-                                <ul>
-                                    <li>${order.totalPrice}</li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div className="blog-footer">
-                            <ul>
-                                <li>{order.startDate} - {order.endDate}</li>
-                                <li className={order.status}>{order.status}</li>
-
-
-                                {/* <li className="comments"><a href="#"><svg className="icon-bubble"><use xlink: href="#icon-bubble"></use></svg><span className="numero">4</span></a></li> */}
-                                {/* <li className="shares"><a href="#"><svg className="icon-star"><use xlink: href="#icon-star"></use></svg><span className="numero">1</span></a></li> */}
-                            </ul>
-                        </div>
-
-                    </div>
-                ))}
+                <table className="trip-table">
+                    <thead>
+                        <tr>
+                            <th>Destination</th>
+                            <th>Host</th>
+                            <th>Check-in</th>
+                            <th>Check-out</th>
+                            <th>Total</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orders && orders.map((order, idx) => (
+                            <tr key={idx} onClick={() => handleClick(order.stay._id)}>
+                                <td className="destination-cell">
+                                    <img src={order.stay.imgUrls[0]} alt="Stay Image" />
+                                    <div>
+                                        <div className="stay-name">{order.stay.name}</div>
+                                        <div className="stay-city">{order.stay.city}</div>
+                                    </div>
+                                </td>
+                                <td className="trips-host-name">{order.stay.hostFullname}</td>
+                                <td className="trips-start-date">{order.startDate}</td>
+                                <td className="trips-end-date">{order.endDate}</td>
+                                <td className="trips-price">${order.totalPrice}</td>
+                                <td className={order.status}>{order.status}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </section>
-
-
         </section>
-    )
+    );
 }
