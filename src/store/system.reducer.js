d6b7cf83bf9f9a5dca3bc36ccc467f9ae0bbbf90
OriDalm <oridalmedigos@gmail.com>
2023-11-06 18:37:10 +0200

    Dashboard nav selection is now working properly

--- src/store/system.reducer.js
+++ src/store/system.reducer.js
@@ -13,12 +13,18 @@ export const SET_APP_MODAL_ABOUT = 'SET_APP_MODAL_ABOUT'
 export const SET_APP_MODAL_AMENITIES = 'SET_APP_MODAL_AMENITIES'
 export const SET_APP_MODAL_REVIEWS = 'SET_APP_MODAL_REVIEWS'
 export const CLOSE_APP_MODAL = 'CLOSE_APP_MODAL'
+
+export const SET_NAV_DASHBOARD = 'SET_NAV_DASHBOARD'
+export const SET_NAV_TRIPS = 'SET_NAV_TRIPS'
+export const SET_NAV_WISHLIST = 'SET_NAV_WISHLIST'
+export const SET_NAV_HOMEPAGE = 'SET_NAV_HOMEPAGE'
 const initialState = {
   isLoading: false,
   isFilterExpanded: false,
   isExpandedModalOpen: false,
   appModal: false,
   isFocusedModal: false,
+  dashNav: false
 }
 
 export function systemReducer(state = initialState, action = {}) {
@@ -51,6 +57,14 @@ export function systemReducer(state = initialState, action = {}) {
       return { ...state, appModal: SET_APP_MODAL_AMENITIES }
     case SET_APP_MODAL_REVIEWS:
       return { ...state, appModal: SET_APP_MODAL_REVIEWS }
+      case SET_NAV_DASHBOARD:
+        return {...state, dashNav: SET_NAV_DASHBOARD}
+      case SET_NAV_TRIPS:
+        return {...state, dashNav: SET_NAV_TRIPS}
+      case SET_NAV_WISHLIST:
+        return {...state, dashNav: SET_NAV_WISHLIST}
+      case SET_NAV_HOMEPAGE:
+        return {...state, dashNav: SET_NAV_HOMEPAGE}
     default:
       return state
   }
